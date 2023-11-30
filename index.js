5067037dac3e0ee9eb19f85fcde2ca2187d2d63a
Iamdk21 <118039257+Iamdk21@users.noreply.github.com>
2023-11-30 19:29:33 +0530

    First commit
    Link of Website: https://drummusic694.netlify.app/

---
+++ index.js
@@ -0,0 +1,83 @@
+// Way to use Event Listener in Class
+var noofdrum=document.querySelectorAll(".drum").length;
+for (var i=0;i<noofdrum;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click" , handleclick);
+
+}
+document.addEventListener("keydown",function(event){
+    const key=event.key;
+    animationkey(event.key);
+    switch(key){
+        case "w":
+            var tom1= new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2= new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3= new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4= new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare= new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash= new Audio("sounds/crash.mp3");
+           crash.play();
+            break;
+        case "l":
+            var kick= new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default: (alert("OOPS! you pressed " + key+". Please press key mentioned below."));
+    }
+});
+function handleclick(){
+    var innerHtmlButton=this.innerHTML;
+    animationkey(innerHtmlButton);
+    switch(innerHtmlButton){
+        case "w":
+            var tom1= new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2= new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3= new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4= new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare= new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash= new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick= new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default: (console.log(innerHtmlButton));
+    }
+
+}
+function animationkey(currentkey){
+    var animationKey=document.querySelector('.' + currentkey);
+    animationKey.classList.add("pressed");
+    setTimeout(function()
+        {animationKey.classList.remove("pressed")},100);
+}
